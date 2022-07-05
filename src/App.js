5ae377ad132894a70cea08688ff2c8bed1d357aa
Anna <k_anna_i@mail.ru>
2022-07-05 07:03:35 +0300

    fetch del

--- src/App.js
+++ src/App.js
@@ -1,38 +1,28 @@
 import { useState } from 'react'
-import { useEffect } from 'react'
+//import { useEffect } from 'react'
 import { nanoid } from 'nanoid'
 import './App.css'
 import Note from "./components/Note"
 
 function App() {
 
-  const url = "https://apis.scrimba.com/jsonplaceholder/posts"
-  //const url = "http://localhost:7777/notes"
+  const url = "http://localhost:7777/notes/"
   const [allNotes, setNotes] = useState([])
+  console.log(allNotes)
 
   function load() {
     fetch(url)
       .then(res => res.json())
       .then(data => {
           const nt = data.slice(0,2)
-          const loadNotes = []
           for (let i of nt) {
-              loadNotes.push({key: i.id, id: i.id, text: i.title})
+            if (allNotes.find(note => note.id === i.id)) {
+              continue
+            }
+            setNotes(prev => [...prev, {id: i.id, text: i.title}])
           }
-          //console.log(allNotes)
-          //console.log(loadNotes)
-          setNotes(prev => [...prev, loadNotes])
-          //return loadNotes
       })
   } 
-
-  // fetch ("http://localhost:7777/notes")
-  //   .then(res => res.json())
-  //   .then(data => {
-  //     setNotes(prev => {
-  //       [...prev, JSON.parse(data)]
-  //     })
-  //   })
 
   //const [allNotes, setNotes] = useState(() => JSON.parse(localStorage.getItem("notes")) || [])
 
@@ -48,12 +38,32 @@ function App() {
       text: document.querySelector(".addArea").value
     }
     document.querySelector(".addArea").value = ""
-    setNotes(prev => [...prev, add])
+    //setNotes(prev => [...prev, add])
+
+    const options = {
+      method: "POST",
+      body: JSON.stringify(add),
+      headers: {"Content-Type": "application/json"}
+    }
+
+    fetch(url + add.id, options)
+        .then(res => res.json())
+        .then(data => {
+          setNotes(prev => [...prev, data])
+        })
   }
 
   function delNote(event, noteId) {
     event.stopPropagation()
     setNotes(prev => prev.filter(note => note.id !== noteId))
+
+    const options = {
+      method: "DELETE"
+    }
+
+    fetch(url + noteId, options)
+        .then(res => res.json())
+        .then(data => {console.log(data)})
   }
 
   const notes = allNotes.map(note => {
